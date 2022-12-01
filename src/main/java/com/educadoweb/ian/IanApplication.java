140d35740914426bf2d7f0fe2486f2d62ba0cccd
Ian Rodrigues <ianrodrigues107@gmail.com>
2022-12-01 15:23:55 -0300

    Project created

---
+++ src/main/java/com/educadoweb/ian/IanApplication.java
@@ -0,0 +1,13 @@
+package com.educadoweb.ian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educadoweb/ian/IanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educadoweb.ian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
